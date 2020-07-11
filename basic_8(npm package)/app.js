58fdbf30c9a463de61f1829941c0f2a8b56b6856
tahmid526628 <42012470+tahmid526628@users.noreply.github.com>
2020-07-12 04:29:58 +0600

    Update app.js

--- basic_8(npm package)/app.js
+++ basic_8(npm package)/app.js
@@ -1,1 +1,11 @@
+const _ = require("lodash");
+
+let example = _.fill([1,2,3,4,5], "Tahmid", 1, 4);
+console.log(example);
+
+
+// by the way module variable je kono kisu hote paare
+// ami ekhane _ disi
+// chaile lodash ba onno kisu use kora jete paare
+// eta shob module er jonno projojjo
 
